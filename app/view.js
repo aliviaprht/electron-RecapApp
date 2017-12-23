7a2622b67b36e58f3a5c496f38d365a3a915ce2d
harumlokawati <harumlokawati@gmail.com>
2017-12-23 14:27:03 +0700

    add model,view,renderer

---
+++ app/view.js
@@ -0,0 +1,47 @@
+'use strict'
+const path = require('path')
+const model = require(path.join(__dirname, 'model.js'))
+
+module.exports.showOrder = function (rowsObject) {
+  let markup = ''
+  for (let rowId in rowsObject) {
+    let row = rowsObject[rowId]
+    markup += '<div class="box-' + row.warna + '">' + 
+    '<p class="font-weight-bold">' + row.nama_konsumen + '</p><p>' +
+    row.jumlah + ' potong</p>' + '<button type="button" class="btn btn-primary btn-sm"  data-toggle="modal"' +
+    'id="detail_' + row.id_order + '" data-target="#modalDetail_' + row.id_order + '">' + 'See Details </button>' +
+    '<button type="button" class="btn btn-primary btn-sm"  data-toggle="modal"' +
+    'id="detail_' + row.id_order + '" data-target="#modalNext_' + row.id_order + '">' + 'Next </button>'
+    let proses = ''
+    switch(row.proses){
+      case 0: proses = '#LKO'; break;
+      case 1: proses = '#TUNGGU_KAIN'; break;
+      case 2: proses = '#POTONG'; break;
+      case 3: proses = '#FILM'; break;
+      case 4: proses = '#SABLON'; break;
+      case 5: proses = '#JAHIT'; break;
+      case 6: proses = '#PACKING'; break;
+      case 7: proses = '#BELUM_KIRIM'; break;
+      case 8: proses = '#SUDAH_KIRIM';
+    }
+    $(proses).append(markup);
+  }
+  $('#order-list button.detail').each(function (idx, obj) {
+    $(obj).on('click', function () {
+      window.view.showModul1(this.id)
+    })
+  })
+  $('#order-list button.next').each(function (idx, obj) {
+    $(obj).on('click', function () {
+      window.view.showModul2(this.id)
+    })
+  })
+}
+
+module.exports.showModul1 = function (e) {
+ 
+}
+
+module.exports.showModul2 = function (e) {
+  
+}
--- main.js
+++ main.js
@@ -1,20 +1,54 @@
-const electron = require('electron');
-const url = require ('url');
-const path = require('path');
-
-const {app, BrowserWindow} = electron;
-
+'use strict'
+const electron = require('electron')
+const url = require ('url')
+const path = require('path')
+const {app, BrowserWindow} = electron
+const globalShortcut = electron.globalShortcut
+const os = require('os')
+const config = require(path.join(__dirname, 'package.json'))
+const model = require(path.join(__dirname, 'app', 'model.js'))
 let mainWindow;
 
 //Listen for app to be ready
-app.on('ready', function(){
+app.on('ready', function () {
 	//Create new window
 	mainWindow = new BrowserWindow({});
 	
 	//Load html window
-	mainWindow.loadURL(url.format({
-		pathname : path.join(__dirname,'html/mainPage.html'),
+    model.initDb(app.getPath('userData'),
+    // Load a DOM stub here. See renderer.js for the fully composed DOM.
+    mainWindow.loadURL(url.format({
+		pathname : path.join(__dirname,'app/html/mainPage.html'),
 		protocol : 'file:',
 		slashes: true
-	}));
-});
+	}))
+  )
+
+  // Enable keyboard shortcuts for Developer Tools on various platforms.
+  let platform = os.platform()
+  if (platform === 'darwin') {
+    globalShortcut.register('Command+Option+I', () => {
+      mainWindow.webContents.openDevTools()
+    })
+  } else if (platform === 'linux' || platform === 'win32') {
+    globalShortcut.register('Control+Shift+I', () => {
+      mainWindow.webContents.openDevTools()
+    })
+  }
+
+//   mainWindow.once('ready-to-show', () => {
+//     mainWindow.setMenu(null)
+//     mainWindow.show()
+//   })
+
+//   mainWindow.onbeforeunload = (e) => {
+//     // Prevent Command-R from unloading the window contents.
+//     e.returnValue = false
+//   }
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+})
+
+app.on('window-all-closed', () => { app.quit() })
